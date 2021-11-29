6ce4d476b38b3fefc255adfcb7452acb222ee376
dmytroyevlakhov <yevlakhovdmytro@gmail.com>
2021-11-29 20:39:05 +0200

    Added base config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ProjectsModule } from './projects/projects.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ProjectsModule,
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       installSubscriptionHandlers: true,
       autoSchemaFile: 'schema.gql',
-      debug: true,
-      playground: true,
+      debug: process.env.NODE_ENV === 'development',
+      playground: process.env.NODE_ENV === 'development',
     }),
+    ProjectsModule,
   ],
 })
 export class AppModule {}
